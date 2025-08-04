208efe676e16396dcdce42c1147864fb6e3f8276
Blake Hastings <blakehastings@outlook.com>
2025-08-03 23:50:25 -0500

    - 0-random-walk setup

---
+++ 0-randomness/0-random-walk/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Input;
+
+Console.WriteLine("Hello, World!");
+
+
